46c0ef88417af191dcb8fb76373696d4d4edaa6f
Johannes Krottmayer <krotti83@proton.me>
2026-04-05 01:09:03 +0200

    src/VICy/include: add global include C++ header
    - Add intial global include C++ header.
    
    FILE(S):
    A  src/VICy/include/VICy.hpp
    
    Signed-off-by: Johannes Krottmayer <krotti83@proton.me>

---
+++ src/VICy/include/VICy.hpp
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+/**
+ * VICy - Open Cognitive Assist System
+ *
+ * Copyright (C) 2026 Johannes Krottmayer <krotti83@proton.me>
+ *
+ * This file is part of VICy core modules.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * long with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/**
+ * VICy All classes
+ */
+
+#ifndef _VICY_HPP
+#define _VICY_HPP
+
+/**
+ * VICy Main classes
+ */
+#include <VICyMain/Context.hpp>
+#include <VICyMain/Options.hpp>
+
+/**
+ * VICy VI Core classes
+ */
+#include <VICyCore/VICyCore.hpp>
+#include <VICyCore/VICyEthos.hpp>
+#include <VICyCore/VICyMemoryGraph.hpp>
+
+#endif
