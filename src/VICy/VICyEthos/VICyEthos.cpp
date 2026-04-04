36a65d29be91b5503b01f7e6ee3a644f0add3c3e
Johannes Krottmayer <krotti83@proton.me>
2026-04-05 01:26:48 +0200

    src/VICy/VICyEthos: add VICyEthos C++ source
    - Add intial VICyEthos classes C++ source.
    
    NOTE:
    This is currently a stub.
    
    FILE(S):
    A  src/VICy/VICyEthos/VICyEthos.cpp
    
    Signed-off-by: Johannes Krottmayer <krotti83@proton.me>

---
+++ src/VICy/VICyEthos/VICyEthos.cpp
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+/**
+ * VICy - Open Cognitive Assist System
+ *
+ * Copyright (C) 2026 Johannes Krottmayer <krotti83@proton.me>
+ *
+ * This file is part of VICy's core modules.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * long with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/**
+ * VICy Ethos classes
+ */
+
+#include <VICyCore/VICyEthos.hpp>
+
+using namespace VICy::VICyCore::VICyEthos;
+
+VICyEthos::VICyEthos()
+{
+
+}
+
+VICyEthos::~VICyEthos()
+{
+
+}
