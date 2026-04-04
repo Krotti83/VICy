3e5b2dc9215831cd336d78c3631b052e50c8e8eb
Johannes Krottmayer <krotti83@proton.me>
2026-04-05 00:29:19 +0200

    src/VICy: add main entry point
    - Add initial VICy main entry point.
    
    FILE(S):
    A  src/VICy/main.cpp
    
    Signed-off-by: Johannes Krottmayer <krotti83@proton.me>

---
+++ src/VICy/main.cpp
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+/**
+ * VICy - Open Cognitive Assist System
+ *
+ * Copyright (C) 2026 Johannes Krottmayer <krotti83@proton.me>
+ *
+ * This file is part of VICy's core modules.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * long with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/**
+ * VICy Program Main Entry Point
+ */
+
+#include <VICyMain/Context.hpp>
+
+using namespace VICy::VICyMain;
+
+Context *ctx = nullptr;
+
+int main(int argc, char *argv[])
+{
+    int ret;
+
+    if (argc > 1) {
+        ctx = new Context(argc, argv);
+    } else {
+        ctx = new Context;
+    }
+
+    ret = ctx->Execute();
+
+    if (ctx != nullptr) {
+        delete ctx;
+    }
+
+    return ret;
+}
